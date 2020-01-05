9b849998f208b98583e146718d38e160b8a8c17f
Binbin Sun <binbin.sun@shopee.com>
2020-01-05 22:28:57 +0800

    chore: remove webpack analyzer

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -112,9 +112,4 @@ if (config.build.productionGzip) {
   )
 }
 
-if (config.build.bundleAnalyzerReport) {
-  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
-}
-
 module.exports = webpackConfig
